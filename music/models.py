1c96b1bc0c8db588c417469f2885efd5a8c1b84e
Aadish Goel <aadishgoel2013@users.noreply.github.com>
2018-05-04 13:13:32 +0530

    Update models.py

--- music/models.py
+++ music/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 class Album(models.Model):
     artist = models.CharField(max_length=250)
